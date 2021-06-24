9032c4ad65d5025c514f77b34cfe0f969963badd
Irasema0915 <52756195+Irasema0915@users.noreply.github.com>
2021-06-24 05:57:43 -0500

    Register

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+     public function registro(Request $request){
+        // o se puede usar con $user = new \App\User();
+        $user = new \App\User();
+        $user->name     = $request->name;
+        $user->email    = $request->email;
+        $user->password = bcrypt($request->password);
+        if($user->save()){
+            return response()->json($user,201);
+        }
+        return response()->json(null,204);
+    }
+
+    public function login(Request $request){
+        $credenciales = ["email"=>$request->email, "password"=>$request->password];
+        if(Auth::once($credenciales)){
+            $token = Str::random(60);
+            $request->user()->forceFill([
+                'api_token' => hash('sha256', $token),    
+            ])->save();
+            return response()->json(['token' => $token],201);
+        }
+        \abort(401);
+    }
+
+    
+}

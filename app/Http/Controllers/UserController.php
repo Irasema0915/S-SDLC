e1d01f4d3aa23849e69e2002adaf8499bc77a3da
Irasema0915 <52756195+Irasema0915@users.noreply.github.com>
2021-06-29 03:22:54 -0500

    prueba pull

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
      public function registro(Request $request){
         // o se puede usar con $user = new \App\User();
-        $user = new \App\User();
+        $user = new \App\Models\User();
         $user->name     = $request->name;
         $user->email    = $request->email;
         $user->password = bcrypt($request->password);
@@ -19,16 +21,9 @@ class UserController extends Controller
     }
 
     public function login(Request $request){
-        $credenciales = ["email"=>$request->email, "password"=>$request->password];
-        if(Auth::once($credenciales)){
-            $token = Str::random(60);
-            $request->user()->forceFill([
-                'api_token' => hash('sha256', $token),    
-            ])->save();
-            return response()->json(['token' => $token],201);
-        }
-        \abort(401);
+        return response()->json(\App\Models\User::all(),200);
+
     }
 
-    
+
 }

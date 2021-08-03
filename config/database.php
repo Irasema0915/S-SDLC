b825147fd5a7e08f68eab4252cac715610dc093c
Irasema0915 <52756195+Irasema0915@users.noreply.github.com>
2021-08-03 14:35:56 -0500

    Escenario 3

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,11 +66,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', '192.168.0.14'),
+            'port' => env('DB_PORT', '5434'),
+            'database' => env('DB_DATABASE', 'laravel'),
+            'username' => env('DB_USERNAME', 'irasemaLaravel'),
+            'password' => env('DB_PASSWORD', 'D474b2UTT'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
